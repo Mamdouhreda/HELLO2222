4f20ea7aae0558effcac757e0868701b82b53f4f
Mamdouh <Mamdohreda@gmail.com>
2022-10-20 22:33:02 +0100

    Initial Commit

---
+++ HELLO2222/HELLO2222App.swift
@@ -0,0 +1,17 @@
+//
+//  HELLO2222App.swift
+//  HELLO2222
+//
+//  Created by Mamdouh on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct HELLO2222App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
